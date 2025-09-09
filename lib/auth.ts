6a0ddd9cbd56123980e409cc943943e38d5c63b7
Sudhanshu088 <sudhanshuneravati088@gmail.com>
2025-09-09 18:53:46 +0530

    feat(auth): add email verification

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,9 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
 
 import { db } from "@/lib/db";
+
+import { sendEmail } from "@/utils/send-email";
+
 import { schema } from "@/schema";
 
 export const auth = betterAuth({
@@ -14,14 +17,47 @@ export const auth = betterAuth({
 
     emailAndPassword: {
         enabled: true,
-        autoSignIn: false 
+        autoSignIn: false,
+        requireEmailVerification: true
+    },
+
+    emailVerification: {
+
+        autoSignInAfterVerification: false,
+
+        sendOnSignUp: true,
+        sendOnSignIn: true,
+
+        sendVerificationEmail: async ({ user, url, token }, request) => {
+            await sendEmail({
+                to: user.email,
+                subject: "Verify your email address",
+                text: `Click the link to verify your email: ${url}`,
+            });
+        },
+
+        onEmailVerification: async (user) => {
+            await sendEmail({
+                to: user.email,
+                subject: "Email verified",
+                text: `Your email has been successfully verified.`,
+            });
+        }
+
     },
 
     socialProviders: {
+
         github: {
             clientId: process.env.GITHUB_CLIENT_ID as string,
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
         },
+
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+
     },
 
     plugins: [nextCookies()]
